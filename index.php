8f0374fb07520b847a65f441b01d3a46c10e3893
Gustas <augustasbu@gmail.com>
2018-01-16 00:48:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Web Development Job</title>
+        <link rel="stylesheet" href="./style/bootstrap.min.css">
+        <link rel="stylesheet" href="./style/main.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="row ">
+                        <a href="https://www.seb.lt/"><img src="./logo_seb.png" alt="logo_seb.png"></a>
+                        <h1  class="mx-auto">PRISIJUNK</h1>
+                        <h2 class="mx-auto">prie mūsų komandos!</h2>
+                        <h4 class="mx-auto mt-3">Mes ieškome jaunesniojo WEB turinio valdymo specialisto.</h4>
+                    </div>
+
+                    <div class="row mt-4">
+
+                            <div class="row">
+                                <div class="col  ml-5 list">
+                                    <span>Darbo pobūdis:</span>
+                                    <ul class="">
+                                        <li>Interneto svetainių turinio administravimas ir tobulinimas</li>
+                                        <li>html laiškų kūrimas naudojant paruoštus šablonus</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <div class="row mt-5">
+
+                                <div class="col ml-5 list">
+                                    <span>Mes tikimės, kad:</span>
+                                    <ul class="">
+                                        <li>turi HTML ir CSS žinių</li>
+                                        <li>turi patirties dirbant su turinio valdymo sistemomis (Drupal, WordPress...)</li>
+                                        <li>grafinio dizaino patirtis (Adobe Photoshop) būtų privalumas</li>
+                                        <li>turi estetikos pojūtį bei esi pastabus detalėms</li>
+                                        <li>gebi prisiimti atsakomybę už padarytus darbus</li>
+                                        <li>nori kurti papildomą naudą ir gerinti kokybę</li>
+                                        <li>nori mokytis ir tobulėti</li>
+                                        <li>responsive design, user experience, Google Analytics – tau pažįstami terminai</li>
+                                    </ul>
+
+                                </div>
+                                <div class="col list">
+                                    <span>Mes siūlome:</span>
+                                    <ul>
+                                            <li>komandos pagalbą padedant plėtoti įgūdžius ir gilinti žinias</li>
+                                            <li>galimybę įgyvendinti savo idėjas ir atskleisti savo sugebėjimus</li>
+                                            <li>Tavo darbą matys plati vartotojų auditorija</li>
+                                            <li>Patirtis dirbant su kolegomis iš Latvijos ir Estijos</li>
+                                            <li>galimybę matyti, kaip įgyvendinami dideli projektai tarptautinėje organizacijoje</li>
+                                            <li>galimybę derinti darbą ir mokslus, dirbti nuotoliniu būdu</li>
+
+                                    </ul>
+
+                                </div>
+                            </div>
+                            <div class="row mt-4 w-100">
+                                <div class="col ml-5">
+                                    <h6><i>„Nesvarbu, ką norėtum daryti ar kokiais gabumais esi apdovanotas, tiesiog ryžkis.“</i></h6>
+                                    <span class="mt-3 poetas">Johanas Volfgangas Gėtė, vokiečių poetas ir dramaturgas</span>
+                                </div>
+                                <div class="col mt-3">
+                                    <a href="https://www.seb.lt/jaunesnysis-web-specialistas"><button type="button" class="btn btn-lg">Siųsti CV</button></a>
+                                </div>
+
+                            </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
